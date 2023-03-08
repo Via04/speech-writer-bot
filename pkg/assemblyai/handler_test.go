f2edfd9fd85cb9a418090dee04f893c40aa1af47
Ivan Vashchenko <48572088+Via04@users.noreply.github.com>
2023-03-08 15:22:17 +0300

    Update handler_test.go

--- pkg/assemblyai/handler_test.go
+++ pkg/assemblyai/handler_test.go
@@ -2,7 +2,7 @@ package assemblyai
 
 // func TestGetText(t *testing.T) {
 // 	ctx := context.Background()
-// 	text, err := GetText(ctx, "https://api.telegram.org/file/bot6272163518:AAEvSWsUZr8ZB7XqELvCg5N2d_YFhJeunOA/voice/file_4.oga")
+// 	text, err := GetText(ctx, "https://api.telegram.org/file/botTOKEN/voice/file_4.oga")
 // 	if err != nil || text == "" {
 // 		t.Fatalf("GetText want text, got nothing")
 // 	}
